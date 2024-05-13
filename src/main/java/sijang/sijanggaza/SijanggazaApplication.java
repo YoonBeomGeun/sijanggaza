01d38feb086d7f6b89d6988703c62b93613d4efe
YoonBeomGeun <yunbumkun36@naver.com>
2024-05-13 23:59:35 +0900

    no message

---
+++ src/main/java/sijang/sijanggaza/SijanggazaApplication.java
@@ -0,0 +1,13 @@
+package sijang.sijanggaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SijanggazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SijanggazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sijang/sijanggaza/SijanggazaApplicationTests.java
@@ -0,0 +1,13 @@
+package sijang.sijanggaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SijanggazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

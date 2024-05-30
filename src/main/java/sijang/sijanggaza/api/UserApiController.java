468d0905de8c8f21897e19aadd9143c1cba2bb5c
YoonBeomGeun <yunbumkun36@naver.com>
2024-05-31 00:21:21 +0900

    users api 등록 완료(수정 필요)

--- src/main/java/sijang/sijanggaza/api/UserApiController.java
+++ src/main/java/sijang/sijanggaza/api/UserApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sijang.sijanggaza.domain.SiteUser;
+import sijang.sijanggaza.dto.OrderDto;
 import sijang.sijanggaza.dto.user.UserDto;
 import sijang.sijanggaza.service.UserService;
 
@@ -19,10 +20,18 @@ public class UserApiController {
     @GetMapping("/api/v1/users")
     public List<UserDto> findUserV1() {
         List<SiteUser> findUsers = userService.findAll();
-        List<UserDto> collect =findUsers.stream()
-                .map(u -> new UserDto(u.getId(), u.getUsername(), u.getPassword(), u.getEmail(), u.getOrderList(), u.getStatus()))
+
+        List<UserDto> collect = findUsers.stream()
+                .map(u -> new UserDto(
+                        u.getId(),
+                        u.getUsername(),
+                        u.getPassword(),
+                        u.getEmail(),
+                        u.getOrderList().stream()
+                                .map(o -> new OrderDto(o.getId(), o.getOrderPrice(), o.getQuantity(), o.getOrderDate(), o.getItem(), o.getStatus()))
+                                .collect(Collectors.toList()),
+                        u.getStatus()))
                 .collect(Collectors.toList());
         return collect;
     }
-
 }
--- src/main/java/sijang/sijanggaza/dto/OrderDto.java
+++ src/main/java/sijang/sijanggaza/dto/OrderDto.java
@@ -21,11 +21,5 @@ public class OrderDto {
     private int quantity;
     private LocalDateTime orderDate;
     private Item item;
-    private UserDto userDto;
     private OrderStatus status; //주문 상태(ORDER, CANCEL)
-
-    public void setUserDto(UserDto userDto) {
-        this.userDto = userDto;
-        userDto.getOrderList().add(this);
-    }
 }
